6b9c50e5a62f3a8b8a3c2476950dce52c14f20d5
Ievgen Gavrysh <zhenya.ga@gmail.com>
2019-08-24 18:42:04 -0700

    add docker commands

---
+++ docker/create_image_and_run_contrainer.sh
@@ -0,0 +1,15 @@
+# craete an image
+# spring-boot-web-0.0.1-SNAPSHOT.jar must be in the tmp folder (dot - to look in the current dir for the Dockerfile)
+docker build -t spring-boot-docker .
+
+
+# run container
+docker run -d -p 8080:8080 spring-boot-docker
+
+
+#run container without exiting it
+docker run -d -p 8080:8080 spring-boot-docker tail -f /dev/null
+
+
+# Loging interactively to docker
+docker exec -it <docker_name> bash
